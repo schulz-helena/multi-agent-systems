575f96e8926b3112f35022056e090f9087227b45
schulz-helena <schulz-helena@gmx.de>
2024-04-04 16:34:12 +0200

    Give agents random destinations, let them move there (without minding collisions)

--- src/agent.py
+++ src/agent.py
@@ -7,12 +7,26 @@ class MyAgent(Agent):
 
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
-        
         self.pos = choice(list(self.model.map.nodes()))
+        self.destination = choice(list(self.model.map.nodes()))
+        print(f"Agent {str(self.unique_id)} has destination: {self.destination}.")
+        self.path = []
+        self.present = True
+
         self.model.positions[self.unique_id] = self.pos
-        print(f"I am agent {str(self.unique_id)} and I'm starting at node {self.pos}.")
+        print(f"Agent {str(self.unique_id)} start position: {self.pos}.")
 
     def step(self):
-        self.pos = choice([n for n in self.model.map.neighbors(self.pos)])
-        self.model.positions[self.unique_id] = self.pos
-        print(f"I am agent {str(self.unique_id)} and I'm currently at node {self.pos}.")
+        if not self.present:
+            print(f"Agent {str(self.unique_id)} left the map.")
+            self.model.positions[self.unique_id] = None
+        else:
+            if not self.path:
+                self.path = nx.shortest_path(self.model.map, source=self.pos, target=self.destination, method="dijkstra")
+                self.path.pop(0) # first node in path is always the current position
+                print(f"Agent {str(self.unique_id)} updated path to: {self.path}.")
+            self.pos = self.path.pop(0)
+            if self.pos == self.destination:
+                self.present = False
+            self.model.positions[self.unique_id] = self.pos
+            print(f"Agent {str(self.unique_id)} current position: {self.pos}.")
--- src/main.py
+++ src/main.py
@@ -2,5 +2,5 @@ from model import MyModel
 
 if __name__ == '__main__':
     model = MyModel(2)
-    for _ in range(4):
+    while any(model.positions):
         model.step()
